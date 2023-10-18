05b31b2ed40a2874d5a7ad80c107a21cb9679416
Ricardo Polanco <polancoblancoricardo>
2023-10-17 21:25:23 -0600

    :memo: sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Sidebar from './layout/Sidebar.jsx/Sidebar'
+import Content from './layout/Content/Content'
 
 function App() {
 
   return (
     <>
       <div className='app'>
+        <Sidebar/>
+        <Content/>
       </div>
     </>
   )
--- src/layout/Sidebar.jsx/Sidebar.jsx
+++ src/layout/Sidebar.jsx/Sidebar.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import "./Sidebar.css";
+import {personsImgs} from "../../utils/images";
+import {navigationLinks} from "../../data/data";
 
 const Sidebar = () =>{
     return(
-        <div></div>
+        <div className={'sidebar'}>
+            <div className="user-info">
+                <div className="info-img img-fit-cover">
+                    <img src={personsImgs.person_two} alt="profile" />
+                </div>
+                <span className='info-name'>alice-doe</span>
+            </div>
+            <nav className="navigation">
+                <ul className="nav-list">
+                    {
+                        navigationLinks.map((navigationLinks)=>(
+                            <li className='nav-item' key={navigationLinks.id}>
+                                <a href="#" className={'nav-link'}>
+                                    <img src={navigationLinks.image} className='nav-link-icon' alt={navigationLinks.title}/>
+                                    <span className='nav-link-text'>{navigationLinks.title}</span>
+                                </a>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </nav>
+        </div>
     )
 }
 
